738931fe179136c66f761a39d9a924dd22a406c7
PRATHAM RAJ CHOHLA <prathamchohla@gmail.com>
2025-11-06 17:07:26 +0530

    Update main.py

--- blog-dev/main.py
+++ blog-dev/main.py
@@ -246,4 +246,4 @@ def contact():
 
 # ---------------------------- RUN APP ---------------------------- #
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(host='0.0.0.0', port=5000, debug=True)
